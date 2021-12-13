9a1004094ca2a1c3bf20ae50eca803a526ecc55b
DongGeon0908 <50691225+DongGeon0908@users.noreply.github.com>
2021-12-14 00:44:12 +0900

    :sparkles: diary에서 발생하는 exception 구현

--- src/main/java/com/jungdam/error/ErrorMessage.java
+++ src/main/java/com/jungdam/error/ErrorMessage.java
@@ -12,7 +12,10 @@ public enum ErrorMessage {
     INVALID_REFRESH_TOKEN(HttpStatus.BAD_REQUEST, "토큰에 문제가 발생"),
     FAILURE_FILE_CONVERT(HttpStatus.BAD_REQUEST, "File convert fail"),
     INVALID_DIARY_TITLE(HttpStatus.BAD_REQUEST, "잘못된 일기 제목입니다."),
-    INVALID_DIARY_CONTENT(HttpStatus.BAD_REQUEST, "잘못된 일기 본문입니다.");
+    INVALID_DIARY_CONTENT(HttpStatus.BAD_REQUEST, "잘못된 일기 본문입니다."),
+    NOT_EXIST_ALBUM(HttpStatus.BAD_REQUEST, "존재하지 않는 앨범입니다."),
+    DUPLICATION_DIARY_RECORDED_AT(HttpStatus.BAD_REQUEST, "해당 날짜에 일기가 존재합니다."),
+    NOT_EXIST_PARTICIPANT(HttpStatus.BAD_REQUEST, "앨범에 포함되지 않은 회원입니다.");
 
     private final HttpStatus status;
     private final String message;
