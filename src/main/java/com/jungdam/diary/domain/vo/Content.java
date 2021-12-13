19586901c0fd51a85cd61948ebdac2ca9962b23e
DongGeon0908 <50691225+DongGeon0908@users.noreply.github.com>
2021-12-13 16:50:48 +0900

    :sparkles: Diary VO Validate 로직 추가

--- src/main/java/com/jungdam/diary/domain/vo/Content.java
+++ src/main/java/com/jungdam/diary/domain/vo/Content.java
@@ -1,10 +1,17 @@
 package com.jungdam.diary.domain.vo;
 
+import com.jungdam.error.ErrorMessage;
+import com.jungdam.error.exception.InvalidArgumentException;
+import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
+import javax.persistence.Transient;
 
 @Embeddable
 public class Content {
+
+    @Transient
+    private static final int CONTENT_VALIDATOR = 0;
 
     @Column(name = "diary_content")
     private String content;
@@ -14,6 +21,17 @@ public class Content {
     }
 
     public Content(String content) {
+        validate(content);
         this.content = content;
+    }
+
+    private void validate(String content) {
+        if (Objects.isNull(content) || content.length() <= CONTENT_VALIDATOR) {
+            throw new InvalidArgumentException(ErrorMessage.INVALID_DIARY_CONTENT);
+        }
+    }
+
+    public String getContent() {
+        return content;
     }
 }
--- src/main/java/com/jungdam/error/ErrorMessage.java
+++ src/main/java/com/jungdam/error/ErrorMessage.java
@@ -10,7 +10,9 @@ public enum ErrorMessage {
     FAIL_TO_GENERATE_TOKEN(HttpStatus.BAD_REQUEST, "토큰 생성 실패"),
     NOT_EXPIRED_TOKEN_YET(HttpStatus.BAD_REQUEST, "토큰 만료일이 지나지 않았습니다."),
     INVALID_REFRESH_TOKEN(HttpStatus.BAD_REQUEST, "토큰에 문제가 발생"),
-    FAILURE_FILE_CONVERT(HttpStatus.BAD_REQUEST, "File convert fail");
+    FAILURE_FILE_CONVERT(HttpStatus.BAD_REQUEST, "File convert fail"),
+    INVALID_DIARY_TITLE(HttpStatus.BAD_REQUEST, "잘못된 일기 제목입니다."),
+    INVALID_DIARY_CONTENT(HttpStatus.BAD_REQUEST, "잘못된 일기 본문입니다.");
 
     private final HttpStatus status;
     private final String message;
