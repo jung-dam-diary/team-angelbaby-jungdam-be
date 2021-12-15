5a46ea0da12e39efaae71b74fe922005e23a45db
soo5717 <koo5717@naver.com>
2021-12-15 11:23:39 +0900

    :sparkles: 앨범 도메인 getter, builder 구현

--- src/main/java/com/jungdam/album/domain/Album.java
+++ src/main/java/com/jungdam/album/domain/Album.java
@@ -38,12 +38,20 @@ public class Album extends BaseEntity {
     private Participants participants;
 
     @Embedded
-    private Diaries diaries;
-
-    @Embedded
     private Invitations invitations;
 
+    @Embedded
+    private Diaries diaries;
+
     protected Album() {
+    }
+
+    public Album(Long id, Title title, FamilyMotto familyMotto, Thumbnail thumbnail) {
+        this.id = id;
+        this.title = title;
+        this.familyMotto = familyMotto;
+        this.thumbnail = thumbnail;
+        this.participants = new Participants();
     }
 
     public void addParticipant(Participant participant) {
@@ -63,5 +71,68 @@ public class Album extends BaseEntity {
 
     public Long getId() {
         return id;
+    }
+
+    public static AlbumBuilder builder() {
+        return new AlbumBuilder();
+    }
+
+    public Title getTitle() {
+        return title;
+    }
+
+    public FamilyMotto getFamilyMotto() {
+        return familyMotto;
+    }
+
+    public Thumbnail getThumbnail() {
+        return thumbnail;
+    }
+
+    public String getTitleValue() {
+        return title.getTitle();
+    }
+
+    public String getFamilyMottoValue() {
+        return familyMotto.getFamilyMotto();
+    }
+
+    public String getThumbnailValue() {
+        return thumbnail.getThumbnail();
+    }
+
+    public static class AlbumBuilder {
+
+        private Long id;
+        private Title title;
+        private FamilyMotto familyMotto;
+        private Thumbnail thumbnail;
+
+        private AlbumBuilder() {
+        }
+
+        public AlbumBuilder id(final Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public AlbumBuilder title(final Title title) {
+            this.title = title;
+            return this;
+        }
+
+        public AlbumBuilder familyMotto(final FamilyMotto familyMotto) {
+            this.familyMotto = familyMotto;
+            return this;
+        }
+
+        public AlbumBuilder thumbnail(final Thumbnail thumbnail) {
+            this.thumbnail = thumbnail;
+            return this;
+        }
+
+        public Album build() {
+            return new Album(this.id, this.title, this.familyMotto, this.thumbnail);
+        }
     }
 }
