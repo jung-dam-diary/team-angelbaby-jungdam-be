16dd1f0f2ee2309ceea2d86476ffb563c2c9e6a9
dlfdyd96 <1yongs_@naver.com>
2021-12-15 16:22:40 +0900

    :memo: Swagger 문서화

--- src/main/java/com/jungdam/participant/presentation/ParticipantController.java
+++ src/main/java/com/jungdam/participant/presentation/ParticipantController.java
@@ -6,12 +6,15 @@ import com.jungdam.common.utils.SecurityUtils;
 import com.jungdam.participant.dto.bundle.ReadAllParticipantBundle;
 import com.jungdam.participant.dto.response.ReadAllParticipantResponse;
 import com.jungdam.participant.facade.ParticipantFacade;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api("Participant")
 @RestController
 @RequestMapping("/api/v1/albums/{albumId}/participants")
 public class ParticipantController {
@@ -22,6 +25,7 @@ public class ParticipantController {
         this.participantFacade = participantFacade;
     }
 
+    @ApiOperation("멤버 리스트 조회")
     @GetMapping()
     public ResponseEntity<ResponseDto<ReadAllParticipantResponse>> read(
         @PathVariable Long albumId) {
