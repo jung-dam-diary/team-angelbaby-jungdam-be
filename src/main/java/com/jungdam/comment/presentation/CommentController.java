65eddf358e1ffe06699d75e2e670e404ca0ed9b3
DongGeon0908 <50691225+DongGeon0908@users.noreply.github.com>
2021-12-16 19:37:19 +0900

    :bug: 어노테이션 수정

--- src/main/java/com/jungdam/comment/presentation/CommentController.java
+++ src/main/java/com/jungdam/comment/presentation/CommentController.java
@@ -12,15 +12,15 @@ import com.jungdam.common.utils.SecurityUtils;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Api("Comment")
-@Controller
+@RestController
 @RequestMapping("/api/v1/albums/{albumId}/diaries/{diaryId}/comments")
 public class CommentController {
 
