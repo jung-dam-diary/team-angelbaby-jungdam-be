cc764f62ba01580b8fcdebafb38fedc8cd356bc2
DongGeon0908 <50691225+DongGeon0908@users.noreply.github.com>
2021-12-20 21:11:40 +0900

    :recycle: Message 분류 수정

--- src/main/java/com/jungdam/error/ErrorMessage.java
+++ src/main/java/com/jungdam/error/ErrorMessage.java
@@ -5,46 +5,74 @@ import org.springframework.http.HttpStatus;
 public enum ErrorMessage {
 
     /**
-     * AUTH EXCEPTION
-     **/
-    FAIL_TO_GENERATE_TOKEN("A001", HttpStatus.BAD_REQUEST, "토큰 생성 실패"),
-    NOT_EXPIRED_TOKEN_YET("A002", HttpStatus.BAD_REQUEST, "토큰 만료일이 지나지 않았습니다."),
-    INVALID_REFRESH_TOKEN("A003", HttpStatus.BAD_REQUEST, "토큰에 문제가 발생"),
-    UNAUTHORIZED_REDIRECT_URI("A004", HttpStatus.UNAUTHORIZED,
+     * AUTH ERROR RESPONSE MESSAGE
+     */
+    FAIL_TO_GENERATE_TOKEN("E-AU001", HttpStatus.BAD_REQUEST, "토큰 생성 실패"),
+    NOT_EXPIRED_TOKEN_YET("E-AU002", HttpStatus.BAD_REQUEST, "토큰 만료일이 지나지 않았습니다."),
+    INVALID_REFRESH_TOKEN("E-AU003", HttpStatus.BAD_REQUEST, "토큰에 문제가 발생"),
+    UNAUTHORIZED_REDIRECT_URI("E-AU004", HttpStatus.UNAUTHORIZED,
         "Unauthorized Redirect URI and can't proceed with the authentication"),
-    FAIL_TO_LOGIN_OAUTH2("A005", HttpStatus.UNAUTHORIZED, "OAUTH2 로그인을 실패했습니다."),
-    NOT_EXIST_MEMBER("A006", HttpStatus.NOT_FOUND, "존재하지 않는 회원입니다."),
+    FAIL_TO_LOGIN_OAUTH2("E-AU005", HttpStatus.UNAUTHORIZED, "OAUTH2 로그인을 실패했습니다."),
 
     /**
-     * FILE EXCEPTION
+     * MEMBER ERROR RESPONSE MESSAGE
      **/
-    FAILURE_FILE_CONVERT("S001", HttpStatus.BAD_REQUEST, "File convert fail"),
+    NOT_EXIST_PROVIDER_TYPE("E-M007", HttpStatus.BAD_REQUEST, "존재하지 않는 제공자 타입입니다."),
+    INVALID_MEMBER_NICKNAME("E-M008", HttpStatus.BAD_REQUEST, "사용자 닉네임 정보가 잘못되었습니다."),
+    NOT_EXIST_MEMBER("E-M006", HttpStatus.NOT_FOUND, "존재하지 않는 회원입니다."),
 
     /**
-     * COMMON EXCEPTION
+     * ALBUM ERROR RESPONSE MESSAGE
      **/
-    INVALID_INPUT_VALUE("C001", HttpStatus.BAD_REQUEST, "잘못된 입력입니다."),
-    NO_PERMISSION_INVITATION_UPDATE("C002", HttpStatus.FORBIDDEN, "초대 수정 권한이 없습니다."),
-    NOT_EXIST_PROVIDER_TYPE("C003", HttpStatus.BAD_REQUEST, "존재하지 않는 제공자 타입입니다."),
-    INVALID_DIARY_TITLE("C004", HttpStatus.BAD_REQUEST, "잘못된 일기 제목입니다."),
-    INVALID_DIARY_CONTENT("C005", HttpStatus.BAD_REQUEST, "잘못된 일기 본문입니다."),
-    NOT_EXIST_ALBUM("C006", HttpStatus.BAD_REQUEST, "존재하지 않는 앨범입니다."),
-    DUPLICATION_DIARY_RECORDED_AT("C007", HttpStatus.BAD_REQUEST, "해당 날짜에 일기가 존재합니다."),
-    NOT_EXIST_PARTICIPANT("C008", HttpStatus.BAD_REQUEST, "앨범에 포함되지 않은 회원입니다."),
-    NOT_EXIST_DIARY("C009", HttpStatus.BAD_REQUEST, "존재하지 않는 일기입니다."),
-    DUPLICATION_PARTICIPANT_IN_ALBUM("C010", HttpStatus.BAD_REQUEST, "해당 앨범에 회원이 존재합니다."),
-    DUPLICATION_INVITATION_IN_ALBUM("C011", HttpStatus.BAD_REQUEST, "해당 앨범에 회원을 초대 후 대기 상태입니다."),
-    INVALID_INVITATION_STATUS("C012", HttpStatus.BAD_REQUEST, "형식에 맞지 않는 초대 상태입니다."),
-    INVALID_COMMENT_CONTENT("C013", HttpStatus.BAD_REQUEST, "댓글의 형식이 잘못되었습니다."),
-    INVALID_DIARY_PHOTO_IMAGE("C014", HttpStatus.BAD_REQUEST, "이미지 정보가 잘못되었습니다."),
-    INVALID_ALBUM_TITLE("C015", HttpStatus.BAD_REQUEST, "형식에 맞지 않는 앨범 제목입니다."),
-    INVALID_ALBUM_FAMILY_MOTTO("C016", HttpStatus.BAD_REQUEST, "형식에 맞지 않는 앨범 가훈입니다."),
-    INVALID_ALBUM_THUMBNAIL("C017", HttpStatus.BAD_REQUEST, "형식에 맞지 않는 앨범 썸네일입니다."),
-    INVALID_DIARY_RECORDED_AT("C018", HttpStatus.BAD_REQUEST, "작성되는 날짜가 현재 날짜보다 미래입니다."),
-    NON_PERMISSION_ALBUM("C019", HttpStatus.BAD_REQUEST, "앨범에 대한 권한이 없습니다."),
-    NOT_EXIST_COMMENT("C020", HttpStatus.NOT_FOUND, "존재하지 않는 댓글입니다."),
-    INVALID_EMOJI_CONTENT("C021", HttpStatus.NOT_FOUND, "존재하지 않는 이모지입니다."),
-    INVALID_MEMBER_NICKNAME("C022", HttpStatus.BAD_REQUEST, "사용자 닉네임 정보가 잘못되었습니다."),
+    INVALID_ALBUM_TITLE("E-A001", HttpStatus.BAD_REQUEST, "형식에 맞지 않는 앨범 제목입니다."),
+    INVALID_ALBUM_FAMILY_MOTTO("E-A002", HttpStatus.BAD_REQUEST, "형식에 맞지 않는 앨범 가훈입니다."),
+    NON_PERMISSION_ALBUM("E-A003", HttpStatus.BAD_REQUEST, "앨범에 대한 권한이 없습니다."),
+    INVALID_ALBUM_THUMBNAIL("E-C004", HttpStatus.BAD_REQUEST, "형식에 맞지 않는 앨범 썸네일입니다."),
+    NOT_EXIST_ALBUM("E-C005", HttpStatus.BAD_REQUEST, "존재하지 않는 앨범입니다."),
+
+    /**
+     * COMMENT ERROR RESPONSE MESSAGE
+     **/
+    INVALID_COMMENT_CONTENT("E-C002", HttpStatus.BAD_REQUEST, "댓글의 형식이 잘못되었습니다."),
+    NOT_EXIST_COMMENT("E-C001", HttpStatus.NOT_FOUND, "존재하지 않는 댓글입니다."),
+
+    /**
+     * PARTICIPANT ERROR RESPONSE MESSAGE
+     **/
+    DUPLICATION_PARTICIPANT_IN_ALBUM("E-P001", HttpStatus.BAD_REQUEST, "해당 앨범에 회원이 존재합니다."),
+    NOT_EXIST_PARTICIPANT("E-P002", HttpStatus.BAD_REQUEST, "앨범에 포함되지 않은 회원입니다."),
+
+    /**
+     * COMMON ERROR RESPONSE MESSAGE
+     **/
+    INVALID_INPUT_VALUE("E-CM001", HttpStatus.BAD_REQUEST, "잘못된 입력입니다."),
+
+    /**
+     * DIARY ERROR RESPONSE MESSAGE
+     **/
+    INVALID_DIARY_TITLE("E-D001", HttpStatus.BAD_REQUEST, "잘못된 일기 제목입니다."),
+    INVALID_DIARY_CONTENT("E-D002", HttpStatus.BAD_REQUEST, "잘못된 일기 본문입니다."),
+    INVALID_DIARY_RECORDED_AT("E-D03", HttpStatus.BAD_REQUEST, "작성되는 날짜가 현재 날짜보다 미래입니다."),
+    INVALID_DIARY_PHOTO_IMAGE("E-D004", HttpStatus.BAD_REQUEST, "이미지 정보가 잘못되었습니다."),
+    NOT_EXIST_DIARY("E-D005", HttpStatus.BAD_REQUEST, "존재하지 않는 일기입니다."),
+    DUPLICATION_DIARY_RECORDED_AT("E-D006", HttpStatus.BAD_REQUEST, "해당 날짜에 일기가 존재합니다."),
+
+    /**
+     * INVITATION ERROR RESPONSE MESSAGE
+     **/
+    DUPLICATION_INVITATION_IN_ALBUM("E-IV001", HttpStatus.BAD_REQUEST, "해당 앨범에 회원을 초대 후 대기 상태입니다."),
+    INVALID_INVITATION_STATUS("E-IV002", HttpStatus.BAD_REQUEST, "형식에 맞지 않는 초대 상태입니다."),
+    NO_PERMISSION_INVITATION_UPDATE("E-IV003", HttpStatus.FORBIDDEN, "초대 수정 권한이 없습니다."),
+
+    /**
+     * EMOJI ERROR RESPONSE MESSAGE
+     **/
+    INVALID_EMOJI_CONTENT("E-E001", HttpStatus.NOT_FOUND, "존재하지 않는 이모지입니다."),
+
+    /**
+     * IMAGE ERROR RESPONSE MESSAGE
+     **/
+    FAILURE_FILE_CONVERT("E-IM001", HttpStatus.BAD_REQUEST, "File convert fail"),
 
     ;
 
