9a10436f6d905308cea5b8081e3ff3eee67449f4
DongGeon0908 <50691225+DongGeon0908@users.noreply.github.com>
2021-12-20 23:20:54 +0900

    :recycle: PageUtil 구현

---
+++ src/main/java/com/jungdam/common/utils/PageUtil.java
@@ -0,0 +1,13 @@
+package com.jungdam.common.utils;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+public class PageUtil {
+
+    private final static int DEFAULT_PAGE = 0;
+
+    public static Pageable of(int size) {
+        return PageRequest.of(DEFAULT_PAGE, size);
+    }
+}
